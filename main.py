8b9ec4eab372381cde2bbb4db7150f1813724b87
NikaEG <ally.via@gmail.com>
2020-07-20 13:56:43 +0000

    Added print lines

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("Line one")
+print("Nika: Line one")
+print("Computer: What a creative line that is!")
+print("Nika: I know, right? I'm practically a genius.")
+print("Computer: practically.")
